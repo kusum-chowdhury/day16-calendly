2462ea38f2cc1b30a865ca883c7e0c5944991df6
kusum chowdhury <kusumchowdhury5832@gmail.com>
2023-02-27 19:32:27 +0530

    created model for scheduling the meeting

---
+++ models/scheduleModel.js
@@ -0,0 +1,32 @@
+const Mongoose = require('mongoose');
+
+const schedulesSchema = new Mongoose.Schema({
+    user: {
+        type: Mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    day: {
+        type: Date,
+        required: true
+    },
+    dayStart: {
+        type: Date,
+        required: true
+    },
+    dayEnd: {
+        type: Date,
+        required: true
+    },
+    eventDuration: {
+        type: Date,
+        required: true
+    },
+    events: [
+        {
+            type: Mongoose.Schema.Types.ObjectId,
+            ref: "Event"
+        }
+    ]
+})
+
+module.exports = Mongoose.model("Schedule", schedulesSchema);
