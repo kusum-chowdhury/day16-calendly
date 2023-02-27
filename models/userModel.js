371c52c912aae2f43070c1352a8ad03ce503094b
kusum chowdhury <kusumchowdhury5832@gmail.com>
2023-02-27 19:31:21 +0530

    createde userModel

---
+++ models/userModel.js
@@ -0,0 +1,31 @@
+
+const Mongoose = require('mongoose');
+
+const userSchema = new Mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    }, 
+    password: {
+        type: String,
+        required: true
+    },
+    events: [
+        {
+            type: Mongoose.Schema.Types.ObjectId,
+            ref: "Event"
+        }
+    ],
+    schedules: [
+        {
+            type: Mongoose.Schema.Types.ObjectId,
+            ref: "Schedule"
+        }
+    ]
+});
+
+module.exports = Mongoose.model("User", userSchema);
