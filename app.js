ec76a659a68261de415de65048c0939a1b91fda3
kusum chowdhury <kusumchowdhury5832@gmail.com>
2023-02-27 19:37:40 +0530

    created server and set routes

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const connectDB = require("./config/db");
+
+require("dotenv").config();
+const userRoutes = require('./routes/userRoutes')
+const scheduleRoutes = require('./routes/scheduleRoutes')
+const eventRoutes = require('./routes/eventRoutes')
+
+connectDB();
+app.set(cors());
+
+app.use('/api/v1/user', userRoutes);
+
+app.use('/api/v1/schedule', scheduleRoutes);
+
+app.use('/api/v1/event', eventRoutes);
+
+
+app.listen(process.env.PORT, () => {
+    console.log("server is running")
+})
